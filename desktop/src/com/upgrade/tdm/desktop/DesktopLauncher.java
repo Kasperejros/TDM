021076cb3f10a013d5332b61df45469ec9f0a8de
Kasperejros <krajewski.kacper@gmail.com>
2019-09-22 16:21:51 +0200

    initial project commit

---
+++ desktop/src/com/upgrade/tdm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.upgrade.tdm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.upgrade.tdm.Game_TDM;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game_TDM(), config);
+	}
+}
