65c7e25376625976f38f6540172defd3d56d2fec
Kasperejros <krajewski.kacper@gmail.com>
2019-09-23 21:05:43 +0200

    added movement and obstacles from tmx map loader

--- core/src/com/upgrade/tdm/screens/PlayScreen.java
+++ core/src/com/upgrade/tdm/screens/PlayScreen.java
@@ -1,6 +1,8 @@
 package com.upgrade.tdm.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -13,6 +15,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.upgrade.tdm.Game_TDM;
+import com.upgrade.tdm.MapLoader;
 import com.upgrade.tdm.ShapeFactory;
 
 public class PlayScreen implements Screen {
@@ -23,6 +26,23 @@ public class PlayScreen implements Screen {
     private final OrthographicCamera camera;
     private final Viewport viewport;
     private final Body player;
+    private final MapLoader mapLoader;
+
+    private static final int MOVE_DIRECTION_NONE = 0;
+    private static final int MOVE_DIRECTION_FORWARD = 1;
+    private static final int MOVE_DIRECTION_BACKWARD = 2;
+
+    private static final int TURN_DIRECTION_NONE = 0;
+    private static final int TURN_DIRECTION_LEFT = 1;
+    private static final int TURN_DIRECTION_RIGHT = 2;
+
+    private static final float DRIFT_FACTOR = 0.0f;
+    private static final float TURN_SPEED = 2.0f;
+    private static final float MOVE_SPEED = 12.0f;
+    private static final float MAX_SPEED = 24.0f;
+
+    private int moveDirection = MOVE_DIRECTION_NONE;
+    private int turnDirection = TURN_DIRECTION_NONE;
 
 
     public PlayScreen() {
@@ -32,7 +52,10 @@ public class PlayScreen implements Screen {
         camera = new OrthographicCamera();
         camera.zoom = 6f;
         viewport = new FitViewport(640 / Game_TDM.PPM, 480 / Game_TDM.PPM, camera);
-        player = ShapeFactory.createRectangle(new Vector2(0,0), new Vector2(64,128), BodyDef.BodyType.DynamicBody, world, 0.4f);
+        mapLoader = new MapLoader(world);
+        player = mapLoader.getPlayer();
+
+        player.setLinearDamping(0.5f);
     }
     @Override
     public void show() {
@@ -44,7 +67,10 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        handleInput();
+        processInput();
         update(delta);
+        handleDrift();
         draw();
 
     }
@@ -55,11 +81,88 @@ public class PlayScreen implements Screen {
     }
 
     private void update(final float delta) {
+        Vector2 baseVector = new Vector2(0,0);
+
+
+
         camera.position.set(player.getPosition(), 0);
         camera.update();
 
         world.step(delta, 6, 2); // TODO check what those variables do
 
+    }
+
+    private void processInput() {
+
+        Vector2 baseVector = new Vector2(0,0);
+
+        if (turnDirection == TURN_DIRECTION_RIGHT){
+            player.setAngularVelocity(-TURN_SPEED);
+        } else if (turnDirection == TURN_DIRECTION_LEFT){
+            player.setAngularVelocity(TURN_SPEED);
+        } else if (turnDirection == TURN_DIRECTION_NONE && player.getAngularVelocity() != 0){
+            player.setAngularVelocity(0.0f);
+        }
+
+        if (moveDirection == MOVE_DIRECTION_FORWARD){
+            baseVector.set(0, MOVE_SPEED);
+        } else if (moveDirection == MOVE_DIRECTION_BACKWARD) {
+            baseVector.set(0, -MOVE_SPEED);
+        }
+
+        if (!baseVector.isZero() && player.getLinearVelocity().len() < MAX_SPEED){
+            player.applyForceToCenter(player.getWorldVector(baseVector), true);
+        }
+    }
+
+    private void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)){
+            moveDirection = MOVE_DIRECTION_FORWARD;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            moveDirection = MOVE_DIRECTION_BACKWARD;
+        } else {
+            moveDirection = MOVE_DIRECTION_NONE;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            turnDirection = TURN_DIRECTION_LEFT;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            turnDirection = TURN_DIRECTION_RIGHT;
+        } else {
+            turnDirection = TURN_DIRECTION_NONE;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
+            Gdx.app.exit();
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)){
+            camera.zoom -= 0.4f;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.E)){
+            camera.zoom += 0.4f;
+        }
+    }
+
+    private Vector2 getForwardVelocity(){
+        Vector2 currentNormal = player.getWorldVector(new Vector2(0, 1));
+        float dotProduct = currentNormal.dot(player.getLinearVelocity());
+        return multiply(dotProduct, currentNormal);
+    }
+
+    private Vector2 getLateralVelocity(){
+        Vector2 currentNormal = player.getWorldVector(new Vector2(1, 0));
+        float dotProduct = currentNormal.dot(player.getLinearVelocity());
+        return multiply(dotProduct, currentNormal);
+    }
+
+    private Vector2 multiply(float a, Vector2 v){
+        return new Vector2(a * v.x, a * v.y);
+    }
+
+    private void handleDrift() {
+        Vector2 forwardSpeed = getForwardVelocity();
+        Vector2 lateralSpeed = getLateralVelocity();
+        player.setLinearVelocity(forwardSpeed.x + lateralSpeed.x * DRIFT_FACTOR, forwardSpeed.y + lateralSpeed.y * DRIFT_FACTOR);
     }
 
     @Override
@@ -87,5 +190,6 @@ public class PlayScreen implements Screen {
         batch.dispose();
         world.dispose();
         debugRenderer.dispose();
+        mapLoader.dispose();
     }
 }
